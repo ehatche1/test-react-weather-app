db2912e31a9cc5cec24f8de7db735f5278180328
ehatche1 <140868222+ehatche1@users.noreply.github.com>
2024-06-28 21:54:56 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.linkedin.com/in/egypthatchett626/" target="_blank">
-          {" "}
-          Egypt Hatchett
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/ehatche1/react-weather-app" target="_blank">
-          open sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/egypthatchett626/"
+            target="_blank"
+          >
+            {" "}
+            Egypt Hatchett
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/ehatche1/react-weather-app"
+            target="_blank"
+          >
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
