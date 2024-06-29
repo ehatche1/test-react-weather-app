6df33223ccb2bd71f798f7b8620911e372a191a1
ehatche1 <140868222+ehatche1@users.noreply.github.com>
2024-06-28 21:13:07 -0400

    Added Github repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.linkedin.com/in/egypthatchett626/" target="_blank">
+          {" "}
+          Egypt Hatchett
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/ehatche1/react-weather-app" target="_blank">
+          open sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
